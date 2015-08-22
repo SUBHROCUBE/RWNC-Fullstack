0c0c4a5f2fc7c58ac46ac75a1723d910995682d9
Ninad <ninad.mahajan@tcs.com>
2015-08-22 17:37:57 +0530

    production addition

--- client/app/modules/modules.addToProduction/modules.addToProduction.controller.js
+++ client/app/modules/modules.addToProduction/modules.addToProduction.controller.js
@@ -1,7 +1,9 @@
 'use strict';
 
 angular.module('rwncApp')
-  .controller('ModulesAddToProductionCtrl', function ($scope) {
+  .controller('ModulesAddToProductionCtrl',['$scope','$state', function ($scope,$state) {
   	$scope.$parent.module="production";
-    
-  });
+    $scope.addToProduction=function(){
+    	$state.go('modules.productionDetails');
+    };
+  }]);
--- client/app/modules/modules.productionDetails/modules.productionDetails.controller.js
+++ client/app/modules/modules.productionDetails/modules.productionDetails.controller.js
@@ -2,5 +2,14 @@
 
 angular.module('rwncApp')
   .controller('ModulesProductionDetailsCtrl', function ($scope) {
-    $scope.message = 'Hello';
+  	$scope.$parent.module="production";
+  	$scope.datePicker={expStart:false};  	
+  	$scope.openDatePopUp=function($event,dateVar){
+  		console.log(";;;;")
+  		$event.preventDefault();
+    	$event.stopPropagation();
+    	console.log($scope.datePicker);
+     	$scope.datePicker[dateVar]=true;
+  	}
+    
   });
