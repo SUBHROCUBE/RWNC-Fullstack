a173820162bc73e03231a1657e0be3cb0031b565
sujoy <sujoysett@gmail.com>
2015-11-22 17:41:14 +0530

    prod

--- client/app/config/apiConfig.js
+++ client/app/config/apiConfig.js
@@ -2,7 +2,7 @@
 
 var config={};
 
-config.apiBaseURL='http://52.26.114.107:8888/';
+config.apiBaseURL='http://127.0.0.1:8888/';
 config.api={};
 config.api.recentOrders ='recent_orders';
 config.api.recentReceived ='recent_received';
@@ -12,8 +12,10 @@ config.api.allCustomers='all_customers';
 config.api.customer='customer';
 config.api.allStocks='stock';
 config.api.getTypes='alltypes';
+config.api.getStatuses='status';
 config.api.getMaterial='allmaterials ';
 config.api.received='received';
 config.api.receives='receives';
 config.api.orders="orders";
 config.api.addEditOrder="order";
+config.api.productions ='productions';
--- client/app/modules/modules.addToProduction/modules.addToProduction.controller.js
+++ client/app/modules/modules.addToProduction/modules.addToProduction.controller.js
@@ -1,9 +1,36 @@
 'use strict';
 
 angular.module('rwncApp')
-  .controller('ModulesAddToProductionCtrl',['$scope','$state', function ($scope,$state) {
+  .controller('ModulesAddToProductionCtrl',['$scope','$state','getMasterData','httpRequest', function ($scope,$state,getMasterData,httpRequest) {
   	$scope.$parent.module="production";
     $scope.addToProduction=function(){
     	$state.go('modules.productionDetails');
     };
+
+      $scope.productionFilter={};
+      $scope.pageSize=10;
+      getMasterData.getType()
+      .then(function(data){
+        $scope.types=data;
+      });
+      getMasterData.getMaterial()
+      .then(function(data){
+        $scope.materials=data;
+      });
+
+      $scope.orders = [];
+      var getFilteredOrders = function(filterObj){
+	console.log(filterObj);
+        if(angular.isUndefined(filterObj))
+          filterObj={};
+        var api=config.api.orders;
+        httpRequest.postData(api,filterObj)
+        .then(function(data){
+          $scope.orders=data.data;
+          console.log("orders",$scope.orders);
+        });
+      };
+      getFilteredOrders();
+
+
   }]);
